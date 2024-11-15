c753f4a34abcc6801ec8b5c92c200a6823a4bbe5
GetSweet <>
2024-11-15 19:44:03 +0900

    Update MainActivity.java

--- app/src/main/java/com/learneasily/myapplication/MainActivity.java
+++ app/src/main/java/com/learneasily/myapplication/MainActivity.java
@@ -1,24 +1,23 @@
 package com.learneasily.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager2.widget.ViewPager2;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.learneasily.myapplication.adapter.AdapterViewPager;
 import com.learneasily.myapplication.bottom_nav.FragmentLearn;
 import com.learneasily.myapplication.bottom_nav.FragmentNews;
-import com.learneasily.myapplication.bottom_nav.profile_activitys.FragmentProfile;
 import com.learneasily.myapplication.bottom_nav.FragmentTasks;
+import com.learneasily.myapplication.bottom_nav.profile_activitys.FragmentProfile;
 import com.learneasily.myapplication.start_activitys.LoginActivity;
 
 import java.util.ArrayList;
